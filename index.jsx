695bf6b1c440f1b0fee578b821c1521e17a48637
mmerga <78653901+mmerga@users.noreply.github.com>
2023-10-21 14:38:25 -0300

    removing excess of methods
    before app was more then 500 lines now less then 300

--- index.jsx
+++ index.jsx
@@ -68,39 +68,26 @@ class App extends React.Component {
   }
 
   handleKeyDown = (event) => {
+    const clickElement = (value) => {
+      for (let index = 0 ;index < this.elementRefs.length ; index ++) {
+        if(this.elementRefs[index].textContent == value){
+          this.elementRefs[index].click();
+        }
+      }
+    }
     try{
       if(event.key == ","){
         const key = '.';
-        for (let index = 0 ;index < this.elementRefs.length ; index ++) {
-          if(this.elementRefs[index].textContent == key){
-            this.elementRefs[index].click();
-          }
-        }
+        clickElement(key)
       }else if(event.key === ' '){
-        for (let index = 0 ;index < this.elementRefs.length ; index ++) {
-          if(this.elementRefs[index].textContent === 'AC'){
-            this.elementRefs[index].click();
-          } 
-        }
+        clickElement('AC')
       }else if(event.key == "Backspace"){
-        for (let index = 0 ;index < this.elementRefs.length ; index ++) {
-          if(this.elementRefs[index].textContent === ''){
-            this.elementRefs[index].click();
-          } 
-        }
+        clickElement('')
       }else if(event.key == 'Enter'){
         const key = '=';
-        for (let index = 0 ;index < this.elementRefs.length ; index ++) {
-          if(this.elementRefs[index].textContent == key){
-            this.elementRefs[index].click();
-          }
-        }
+        clickElement(key)
       }else{
-        for (let index = 0 ;index < this.elementRefs.length ; index ++) {
-          if(this.elementRefs[index].textContent == event.key){
-            this.elementRefs[index].click();
-          }
-        }
+        clickElement(event.key)
       }
     }catch (error) {
       //console.log({error});
@@ -141,6 +128,17 @@ class App extends React.Component {
   }
   /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
   handleEquals = () => { 
+    const catchErr = () => {
+      //console.error({error, message: 'not a valid entry'});
+      this.setState((prevState) =>  (
+        { 
+          displayTop: INIT_VALUE,
+          displayBot: 'FATAL ERROR NOT A VALID ENTRY',
+          previousResult: INIT_VALUE,
+          result: false
+        }
+      ));
+    }
     if(this.state.result == false){
       try {
         const resultado = mathjs.evaluate(this.state.displayTop);
@@ -153,16 +151,7 @@ class App extends React.Component {
           }
         ));
       } catch (error) {
-        //console.error({error, message: 'not a valid entry'});
-        this.setState((prevState) =>  (
-          { 
-            displayTop: INIT_VALUE,
-            displayBot: 'FATAL ERROR NOT A VALID ENTRY',
-            previousResult: INIT_VALUE,
-            result: false
-          }
-        ));
-
+        catchErr();
       }
     }else{
       try {
@@ -175,15 +164,7 @@ class App extends React.Component {
           }
         ));
       } catch (error) {
-        //console.error({error, message: 'not a valid entry'});
-        this.setState((prevState) =>  (
-          { 
-            displayTop: INIT_VALUE,
-            displayBot: 'FATAL ERROR NOT A VALID ENTRY',
-            previousResult: INIT_VALUE,
-            result: false
-          }
-        ));
+        catchErr();
       }
     }
   }
@@ -207,271 +188,45 @@ class App extends React.Component {
     }
   }
   /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleDivide = () => {
+  handleChooseOperation = (event) => {
+    const operator = event.target.attributes[3].value;
     if(this.state.result == false){
       this.setState((prevState) => (
         { 
-          displayTop: prevState.displayTop + '/',
-          displayBot: '/' 
+          displayTop: prevState.displayTop + operator,
+          displayBot: operator
         }
       ));
     }else{
       this.setState((prevState) => (
         { 
-          displayTop: this.state.previousResult + '/',
-          displayBot: '/' ,
+          displayTop: this.state.previousResult + operator,
+          displayBot: operator, 
           result: false
         }
       ));
     }
-  };
+  }
   /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleMultiply = () => {
+  handleChooseNumber = (event) => {  
+    const num = event.target.attributes[3].value;
     if(this.state.result == false){
       this.setState((prevState) => (
         { 
-          displayTop: prevState.displayTop + '*',
-          displayBot: '*' 
+          displayTop: prevState.displayTop + num,
+          displayBot: prevState.displayBot + num
         }
       ));
     }else{
       this.setState((prevState) => (
         { 
-          displayTop: this.state.previousResult + '*',
-          displayBot: '*' ,
+          displayTop: num,
+          displayBot: num,
           result: false
         }
       ));
     }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleAdd = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '+',
-          displayBot: '+' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: this.state.previousResult + '+',
-          displayBot: '+' ,
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleSubtract = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '-',
-          displayBot: '-' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: this.state.previousResult + '-',
-          displayBot: '-', 
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleNine = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '9',
-          displayBot: prevState.displayBot + '9' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '9',
-          displayBot: '9',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleEight = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '8',
-          displayBot: prevState.displayBot + '8' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '8',
-          displayBot: '8',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleSeven = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '7',
-          displayBot: prevState.displayBot + '7' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '7',
-          displayBot: '7',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleSix = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '6',
-          displayBot: prevState.displayBot + '6' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '6',
-          displayBot: '6',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleFive = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '5',
-          displayBot: prevState.displayBot + '5' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '5',
-          displayBot: '5',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleFour = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '4',
-          displayBot: prevState.displayBot + '4' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '4',
-          displayBot: '4',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleThree = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '3',
-          displayBot: prevState.displayBot + '3' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '3',
-          displayBot: '3',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleTwo = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '2',
-          displayBot: prevState.displayBot + '2' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '2',
-          displayBot: '2',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleOne = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '1',
-          displayBot: prevState.displayBot + '1' 
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '1',
-          displayBot: '1',
-          result: false
-        }
-      ));
-    }
-  };
-  /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
-  handleZero = () => {
-    if(this.state.result == false){
-      this.setState((prevState) => (
-        { 
-          displayTop: prevState.displayTop + '0',
-          displayBot: prevState.displayBot + '0'
-        }
-      ));
-    }else{
-      this.setState((prevState) => (
-        { 
-          displayTop: '0',
-          displayBot: '0',
-          result: false
-        }
-      ));
-    }
-  };
+  }
   /////////////////////////////----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\  
   render() {
     return (
@@ -486,30 +241,30 @@ class App extends React.Component {
             <tr>
               <td id="clear" className="btn-danger" accessKey=' ' onClick={this.handleReset} ref={(element) => this.elementRefs.push(element)}>AC</td>
               <td id="delete" className="btn-warning" accessKey='backspace' onClick={this.handleDelete} ref={(element) => this.elementRefs.push(element)}><FiDelete /></td>
-              <td id="divide" className="btn-dark" accessKey='/' onClick={this.handleDivide} ref={(element) => this.elementRefs.push(element)}>/</td>
-              <td id="multiply" className="btn-dark" accessKey='*' onClick={this.handleMultiply} ref={(element) => this.elementRefs.push(element)}>*</td>
+              <td id="divide" className="btn-dark" accessKey='/' value='/' onClick={this.handleChooseOperation} ref={(element) => this.elementRefs.push(element)}>/</td>
+              <td id="multiply" className="btn-dark" accessKey='*' value='*' onClick={this.handleChooseOperation} ref={(element) => this.elementRefs.push(element)}>*</td>
             </tr>
             <tr>
-              <td id="seven" className="btn-secondary" accessKey='7' onClick={this.handleSeven} ref={(element) => this.elementRefs.push(element)}>7</td>
-              <td id="eight" className="btn-secondary" accessKey='8' onClick={this.handleEight} ref={(element) => this.elementRefs.push(element)}>8</td>
-              <td id="nine" className="btn-secondary" accessKey='9' onClick={this.handleNine} ref={(element) => this.elementRefs.push(element)}>9</td>
-              <td id="subtract" className="btn-dark" accessKey='-' onClick={this.handleSubtract} ref={(element) => this.elementRefs.push(element)}>-</td>
+              <td id="seven" className="btn-secondary" accessKey='7' value='7' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>7</td>
+              <td id="eight" className="btn-secondary" accessKey='8' value='8' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>8</td>
+              <td id="nine" className="btn-secondary" accessKey='9'value='9' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>9</td>
+              <td id="subtract" className="btn-dark" accessKey='-' value='-' onClick={this.handleChooseOperation} ref={(element) => this.elementRefs.push(element)}>-</td>
             </tr>
             <tr>
-              <td id="four" className="btn-secondary" accessKey='4' onClick={this.handleFour} ref={(element) => this.elementRefs.push(element)}>4</td>         
-              <td id="five" className="btn-secondary" accessKey='5' onClick={this.handleFive} ref={(element) => this.elementRefs.push(element)}>5</td>
-              <td id="six" className="btn-secondary" accessKey='6' onClick={this.handleSix} ref={(element) => this.elementRefs.push(element)}>6</td>
-              <td id="add" className="btn-dark" accessKey='+' onClick={this.handleAdd} ref={(element) => this.elementRefs.push(element)}>+</td>
+              <td id="four" className="btn-secondary" accessKey='4' value='4' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>4</td>         
+              <td id="five" className="btn-secondary" accessKey='5' value='5' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>5</td>
+              <td id="six" className="btn-secondary" accessKey='6' value='6' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>6</td>
+              <td id="add" className="btn-dark" accessKey='+' value='+' onClick={this.handleChooseOperation} ref={(element) => this.elementRefs.push(element)}>+</td>
             </tr>
             <tr>
-              <td id="one" className="btn-secondary" accessKey='1' onClick={this.handleOne} ref={(element) => this.elementRefs.push(element)}>1</td>         
-              <td id="two" className="btn-secondary" accessKey='2' onClick={this.handleTwo} ref={(element) => this.elementRefs.push(element)}>2</td>
-              <td id="three" className="btn-secondary" accessKey='3' onClick={this.handleThree} ref={(element) => this.elementRefs.push(element)}>3</td>
+              <td id="one" className="btn-secondary" accessKey='1' value='1' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>1</td>         
+              <td id="two" className="btn-secondary" accessKey='2' value='2' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>2</td>
+              <td id="three" className="btn-secondary" accessKey='3' value='3' onClick={this.handleChooseNumber} ref={(element) => this.elementRefs.push(element)}>3</td>
               <td id="equals" className="btn-success" accessKey='enter' onClick={this.handleEquals} rowSpan="2" ref={(element) => this.elementRefs.push(element)}>=</td>
               <td style={{'display': 'none'}} accessKey='=' ref={(element) => this.elementRefs.push(element)}>=</td>
             </tr>
             <tr>
-              <td id="zero" className="btn-secondary" accessKey='2' onClick={this.handleZero} colSpan="2" ref={(element) => this.elementRefs.push(element)}>0</td>         
+              <td id="zero" className="btn-secondary" accessKey='0' value='0' onClick={this.handleChooseNumber} colSpan="2" ref={(element) => this.elementRefs.push(element)}>0</td>         
               <td id="decimal" className="btn-secondary" accessKey=',' onClick={this.handleFloat} ref={(element) => this.elementRefs.push(element)}>.</td>         
               <td style={{'display': 'none'}} accessKey='.' ref={(element) => this.elementRefs.push(element)}>.</td>         
             </tr>
@@ -524,4 +279,3 @@ document.addEventListener('DOMContentLoaded', function () {
     const MyApp = <App />;    
     ReactDOM.render(MyApp, document.getElementById('wrapper'));
 });
-  
